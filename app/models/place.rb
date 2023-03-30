40170080d714483498fbf877c7ee74804fe6951d
Shunya Nagahori <nagahori.shunya@gmail.com>
2023-03-30 19:09:06 +0900

    set place validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,16 @@ class Place < ApplicationRecord
   has_many_attached :images
 
   validates :place_name, presence: true
+  validates :address, presence: true
+  validates :date_time, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validate :place_date_time_in_advrial
+
+
+  def place_date_time_in_advrial
+    if self.date_time > (self.advrial.end_date + 1) || self.date_time < self.advrial.start_date
+      errors.add(:date_time, :place_date_time_in_advrial)
+    end
+  end
 end
