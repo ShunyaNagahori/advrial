c2207588dcdb48942a86418d80f9f19ade97de5f
Shunya Nagahori <nagahori.shunya@gmail.com>
2023-04-08 21:13:55 +0900

    www

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "advrial_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'advrial.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'www.advrial.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
