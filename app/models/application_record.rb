894bff7960bbcefb2954c1d27f61ee4695364e3b
Shunya Nagahori <nagahori.shunya@gmail.com>
2023-04-15 15:51:47 +0900

    edit application model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,13 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-
-  before_save :set_file_extension
-
-  def set_file_extension
-    if image.attached? && !image.blob.filename.extension
-      mime_type = image.blob.content_type.split("/").last
-      extension = Rack::Mime::MIME_TYPES.invert[mime_type]
-      image.blob.update(filename: "#{image.blob.filename}.#{extension}")
-    end
-  end
 end
