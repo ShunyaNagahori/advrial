6453e41fea23dcdce23971b2aa8a937c323903e5
Shunya Nagahori <nagahori.shunya@gmail.com>
2023-04-15 16:10:27 +0900

    image helper change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 
   # あとで直す
   def image_url_helper(image)
-    Rails.env.development? ? url_for(image) : url_for(image.url)
+    Rails.env.development? ? url_for(image) : image
   end
 end
